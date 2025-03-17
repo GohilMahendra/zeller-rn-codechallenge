3fd0d32cfa74654d98e94d90db18b68bb197e9de
Nehal-Zeller <nehal.sanklecha@myzeller.com>
2025-03-17 10:33:06 +0530

    update API key

--- aws-exports.js
+++ aws-exports.js
@@ -5,7 +5,7 @@ const awsconfig = {
     "https://prrwjjssnvhpbcdwbcwx3nm3zm.appsync-api.ap-southeast-2.amazonaws.com/graphql",
   aws_appsync_region: "ap-southeast-2",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-6y6arb7mwvgrnmds2jignrgr2u",
+  aws_appsync_apiKey: "ak4u34er6ncolceyaycjdjyagy",
 };
 
 export default awsconfig;
